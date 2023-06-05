ad4d2fdebefe59d74e5450cba9f0ed67ccfc7d28
belem <graceitamunoala@gmail.com>
2023-06-05 23:25:02 +0100

    commit

--- Customer/views.py
+++ Customer/views.py
@@ -13,8 +13,18 @@ class Ooder(View):
     def get(self, request, *args, **kwargs):
         # get every item from each category
         appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+        entres = MenuItem.objects.filter(category__name__contains='Entre')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
 
         # pass into context
+        context = {
+            'appetizers': appetizers,
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks
+        }
 
         #render the template
+        return render(request, 'Customer/order.html', context)
     
