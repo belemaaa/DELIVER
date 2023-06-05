e80c0f39595479b0963c1a806a0451283178b68c
belem <graceitamunoala@gmail.com>
2023-06-05 23:14:19 +0100

    commit

--- Customer/views.py
+++ Customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, OrderModel
 
 # Create your views here.
 
@@ -7,3 +8,13 @@ class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'Customer/index.html')
 
+
+class Ooder(View):
+    def get(self, request, *args, **kwargs):
+        # get every item from each category
+        appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+
+        # pass into context
+
+        #render the template
+    
