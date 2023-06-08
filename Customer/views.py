2ac3a45f369fa8cd3506dcb2f50213a32070fb7f
belem <graceitamunoala@gmail.com>
2023-06-08 19:17:52 +0100

    email confirmation

--- Customer/views.py
+++ Customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
 
 # Create your views here.
@@ -68,7 +69,20 @@ class Order(View):
             zip_code=zip_code,
             price=price
             )
-        order.item.add(*item_ids)   
+        order.item.add(*item_ids) 
+
+        # send confirmation email to the user
+        body = ('Thank you for your order! Your food is being made and will be delivered to you soon.\n'
+                f'Your total: {price}\n'
+                'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )  
 
         context = {
             'items': order_items['items'],
