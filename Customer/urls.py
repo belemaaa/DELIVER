4d1af1968e43b1b6141b7434005eba3fbbc8b323
belem <graceitamunoala@gmail.com>
2023-06-08 18:32:30 +0100

    commit

--- Customer/urls.py
+++ Customer/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import Index, Order
-from . import views
 
 urlpatterns = [
     path('', Index.as_view(), name="index"),
     path('order/', Order.as_view(), name="order"),  
-    path('view/orders/', views.ViewOrders, name="view-orders") 
 ]
