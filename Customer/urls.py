7e7fc4a64456545196c698d26aa19892dcd64587
belem <graceitamunoala@gmail.com>
2023-06-12 15:45:17 +0100

    few changes

--- Customer/urls.py
+++ Customer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Index, Order, OrderConfirmation
+from .views import Index, Order, OrderConfirmation, OrderPayConfirmation
 
 urlpatterns = [
     path('', Index.as_view(), name="index"),
     path('order/', Order.as_view(), name="order"), 
-    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(), name="order-confirmation") 
+    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(), name="order-confirmation"), 
+    path('payment-confirmation/', OrderPayConfirmation.as_view(), name="payment-submitted"),
 ]
